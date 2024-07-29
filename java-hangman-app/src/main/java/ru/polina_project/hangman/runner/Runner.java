bce13cca0c1e0b2cda46acf13949d09733aaeb21
polynww <work.ppomogaeva@mail.ru>
2024-07-29 14:18:34 +0300

    новая версия 2

--- java-hangman-app/src/main/java/ru/polina_project/hangman/runner/Runner.java
+++ java-hangman-app/src/main/java/ru/polina_project/hangman/runner/Runner.java
@@ -5,34 +5,36 @@ import ru.polina_project.hangman.game.Game;
 import java.util.Scanner;
 
 public class Runner {
+    private static boolean IS_GAME_ENABLED = true;
+
+    private static void appRunner(Scanner scanner) {
+        int input = scanner.nextInt();
+
+        if (input == 1) {
+            Game.startGame();
+        } else {
+            IS_GAME_ENABLED = false;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean IS_GAME_ENABLED = true;
+
+        if (!IS_GAME_ENABLED) {
+            System.out.println("Game is disabled");
+            return;
+        }
+
+        System.out.println("Do you want to play the game Hangman? (1 - YES, 0 - NO)");
+        appRunner(scanner);
 
 
-        while (IS_GAME_ENABLED) {
-            System.out.println("Do you want to play the game Hangman? (1 - YES, 0 - NO)");
-            int input = scanner.nextInt();
-
-            if (input == 1) {
-                Thread gameThread = new Thread(Game::startGame);
-                gameThread.start();
-                try {
-                    gameThread.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                System.out.println("Game is over. Do you want to play again? (1 - YES, 0 - NO)");
-                int chosenNumber = scanner.nextInt();
-                if (chosenNumber == 0) {
-                    IS_GAME_ENABLED = false;
-                }
-            } else {
-                IS_GAME_ENABLED = false;
-            }
+         while (IS_GAME_ENABLED) {
+            System.out.println("Do you want to play the game Hangman AGAIN? (1 - YES, 0 - NO)");
+            appRunner(scanner);
         }
         System.out.println("Program is finished");
+
         scanner.close();
     }
 }
